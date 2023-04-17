adb0fd335e88355e540442f0339d8dcb52331a8b
Саша Герас <acseler16@gmail.com>
2023-04-17 14:45:03 +0300

    Adding courses list which belong to author

--- XMLEdition/Controllers/AuthorController.cs
+++ XMLEdition/Controllers/AuthorController.cs
@@ -6,8 +6,15 @@ namespace XMLEdition.Controllers
     {
         private Data.AppContext _context = new Data.AppContext();
 
-        public IActionResult Index()
+
+        [Route("/Author/Index/{userId}")]
+        public IActionResult Index(Guid userId)
         {
+            // {5cc5918d-81b7-4a84-bd64-e79fd914ebf7}
+
+            var courses = _context.Courses.Where(c=>c.AuthorId == userId).ToList();
+            ViewBag.Courses = courses;
+
             return View();
         }
 
